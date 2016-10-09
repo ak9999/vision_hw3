9a2079a133cc500f04db8911fb6f6f45ccfc24ad
Abdullah Khan <khanabdullahj@gmail.com>
2016-10-09 19:22:50 -0400

    Add Threshold function

--- image.h
+++ image.h
@@ -79,6 +79,8 @@ bool WriteImage(const std::string &output_filename, const Image &an_image);
 void DrawLine(int x0, int y0, int x1, int y1, int color,
 	      Image *an_image);
 
+void Threshold(Image &an_image);
+
 }  // namespace ComputerVisionProjects
 
 #endif  // COMPUTER_VISION_IMAGE_H_
