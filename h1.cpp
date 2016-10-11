c88400e636207b6e8c1220aa67fe8f75b8e2be2c
Abdullah Khan <khanabdullahj@gmail.com>
2016-10-10 21:51:38 -0400

    Unfinished

--- h1.cpp
+++ h1.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <string>
 #include <cstdlib> // for std::atof
+#include <cmath>
 #include "image.h"
 
 using namespace std;
@@ -25,6 +26,27 @@ int main(int argc, char ** argv)
 
 	const string input(argv[1]);
 	const string output(argv[2]);
+
+	Image img;
+	if (!ReadImage(input, &img)) {
+		cout << "Can\'t read file " << input << endl;
+		return 0;
+	}
+
+	// Create output image with same dimensions as input.
+	Image dst;
+	dst.SetNumberGrayLevels(img.num_gray_levels());
+	dst.AllocateSpaceAndSetSize(img.num_rows(), img.num_columns());
+	for (unsigned int i = 0; i < dst.num_rows(); ++i) // Make dst black.
+		for (unsigned int j = 0; j < dst.num_columns(); ++j)
+			dst.SetPixel(i, j, 0);
+
+	//Sobel(img, img.num_rows(), img.num_columns());
+
+	if (!WriteImage(output, dst)) {
+		cout << "Can\'t write to file." << endl;
+		return 0;
+	}
 
 	return 0;
 }
