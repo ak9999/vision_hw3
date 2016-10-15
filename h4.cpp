600f984e48b7f7676443284cc053f07ea6cc21b5
Abdullah Khan <khanabdullahj@gmail.com>
2016-10-15 14:28:59 -0400

    As far as I'm concerned this part is done.

--- h4.cpp
+++ h4.cpp
@@ -14,6 +14,9 @@
 using namespace std;
 using namespace ComputerVisionProjects;
 
+// Ignore warnings
+#pragma GCC diagnostic ignored "-Wsign-compare"
+
 int main(int argc, char ** argv)
 {
 	if (argc != 5) {
@@ -35,58 +38,61 @@ int main(int argc, char ** argv)
 		return 0;
 	}
 
-	fstream of(array);
-	if (!of.is_open())
+	fstream in(array);
+	if (!in.is_open())
 	{
 		cout << "Could not open " << array << " for writing. Abort." << endl;
 		return 0;
 	}
 
-	int diagonal, width;
+	int hough_height, hough_width;
 
 	// Start reading file.
 	{
 		string str;
-		getline(of, str);
-		diagonal = stoi(str);
-		getline(of, str);
-		width = stoi(str);
+		getline(in, str);
+		hough_height = stoi(str);
+		getline(in, str);
+		hough_width = stoi(str);
 	} // I have read the first two lines of the file.
 
-	vector<pair<int,int>> rho_theta; // rho_theta pairs
-
+	// Init Hough accumulator array.
 	vector<vector<int>> accumulator;
-	accumulator.resize(diagonal, vector<int>(width, 0));
-	for (int i = 0; i < diagonal; i++)
+	accumulator.resize(hough_height, vector<int>(hough_width, 0));
+
+	vector<pair<int,int>> rho_theta; // pixels that will be above threshold
+	for (int i = 0; i < hough_height; i++)
 	{
-		for (int j = 0; j < width; j++)
+		for (int j = 0; j < hough_width; j++)
 		{
 			string str;
-			getline(of, str);
+			getline(in, str);
 			int votes = stoi(str);
 			accumulator[i][j] = votes;
-			cout << votes << endl;
-			if (votes > threshold)
+			if (votes > threshold) // Pixel is significantly bright
 			{
+				// Create std::pair<int,int> and push it into vector
 				rho_theta.push_back(make_pair(i,j));
 			}
 		}
 	}
-	of.close(); // Don't need the file open anymore.
 
 	// Get a vector of lines.
-	auto vector_of_lines = GetLines(rho_theta, img.num_rows(), img.num_columns(), threshold);
+	// auto vector_of_lines = GetLines(rho_theta, img.num_rows(), img.num_columns(), threshold);
+	GetLines(rho_theta, img);
 
-	for (size_t i = 0; i < vector_of_lines.size(); i++)
-	{
-		auto p0 = vector_of_lines[i].first;
-		int x0 = p0.first;
-		int y0 = p0.second;
+	// for (size_t i = 0; i < vector_of_lines.size(); i++)
+	// {
+	// 	auto p0 = vector_of_lines[i].first;
+	// 	int x0 = p0.first;
+	// 	int y0 = p0.second;
 		
-		auto p1 = vector_of_lines[i].second;
-		int x1 = p1.first;
-		int y1 = p1.second;
-	}
+	// 	auto p1 = vector_of_lines[i].second;
+	// 	int x1 = p1.first;
+	// 	int y1 = p1.second;
+
+	// 	DrawLine(x0, y0, x1, y1, 255, &img);
+	// }
 
 	if (!WriteImage(output, img)) {
 		cout << "Can\'t write to file." << endl;
