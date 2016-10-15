ad20561f0605c9bdc8650d8b3e866fd9aaa1dd9c
Abdullah Khan <khanabdullahj@gmail.com>
2016-10-14 20:22:36 -0400

    Re-create accumulator array and hough space image.

--- h4.cpp
+++ h4.cpp
@@ -32,6 +32,42 @@ int main(int argc, char ** argv)
 		return 0;
 	}
 
+	fstream of(array);
+	if (!of.is_open())
+	{
+		cout << "Could not open " << array << " for writing. Abort." << endl;
+		return 0;
+	}
+
+	int diagonal, width;
+
+	// Start reading file.
+	{
+		string str;
+		getline(of, str);
+		diagonal = stoi(str);
+		getline(of, str);
+		width = stoi(str);
+	} // I have read the first two lines of the file.
+
+	// Create accumulator array and re-create Hough space image.
+	Image hough; hough.AllocateSpaceAndSetSize(diagonal, width);
+	hough.SetNumberGrayLevels(255);
+
+	int** accumulator = new int*[diagonal];
+	for (int i = 0; i < diagonal; i++)
+	{
+		accumulator[i] = new int[width];
+		for (int j = 0; j < width; j++)
+		{
+			string str;
+			getline(of, str);
+			int votes = stoi(str);
+			accumulator[i][j] = votes;
+			hough.SetPixel(i, j, votes);
+		}
+	} // Accumulator array obtained!
+
 	// code
 
 	if (!WriteImage(output, img)) {
